10c8c460c697ad2e03c52465fbe5d57b1e4949ac
Mauro <40081963+progettazionemauro@users.noreply.github.com>
2020-03-25 08:54:34 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Messages />
+  </div>
+</template>
+
+<script>
+import Messages from './components/Messages.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Messages
+  }
+}
+</script>
