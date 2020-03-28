0af8c7151a8a37f5e1eb164a302106bc2f98d413
Mauro <40081963+progettazionemauro@users.noreply.github.com>
2020-03-28 08:51:00 +0100

    03-02

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/messages', (req, res) => {
+
+    const messages = ["hello", "hi", "its working"];
+
+    res.send(messages);
+});
+
+app.listen(port, () => console.log('app running'));
---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h4 class="display-1">Messages</h4>
+    <v-list>
+      <v-list-tile v-for="message in messages" @click="">
+        <v-list-tile-content>
+          <v-list-tile-title v-text="message"></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      messages: ["hello", "hi", "its working"]
+    };
+  }
+};
+</script>
