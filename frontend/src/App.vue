288e5a368d0deef171eecab0f0a78853ef59ea2a
Mauro <40081963+progettazionemauro@users.noreply.github.com>
2020-03-29 09:31:27 +0200

    patch

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+
+    </v-toolbar>
+
+    <v-content>
+      <Messages/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Messages from './components/Messages'
+
+export default {
+  name: 'App',
+  components: {
+    Messages
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h4 class="display-1">Messages</h4>
+    <v-list>
+      <v-list-tile v-for="message in messages" @click="">
+        <v-list-tile-content>
+          <v-list-tile-title v-text="message"></v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </div>
+</template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      messages: ["hello", "hi", "its working"]
+    };
+  },
+  async created() {
+    this.messages = (await axios.get('http://localhost:3000/messages')).data;
+  }
+};
+</script>
